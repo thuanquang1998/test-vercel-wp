bf851412f403c68548aa5c37e91b83a07481a60b
thuanluuNetlink <thuan.lq@netlink.vn>
2024-09-02 16:44:45 +0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["mysticthin.xyz"],
+    minimumCacheTTL: 60,
+    unoptimized: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/ads.txt",
+        destination: "/api/txt/ads",
+      },
+      {
+        source: "/:path",
+        destination: "/posts/:path",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
